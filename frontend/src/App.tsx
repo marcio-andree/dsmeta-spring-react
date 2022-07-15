4a57ee94dbb359893cc8809b3a9c9bf7bd0e8665
Marcio André <andremarcio1988@gmail.com>
2022-07-15 08:57:01 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from "./componentes/NotificationButton"
 
 
 function App() {
   return (
-    <h1> Olá Mundo </h1>
+    <>
+      <h1> Olá Mundo </h1>
+      <NotificationButton />
+    </>
   )
 }
 
