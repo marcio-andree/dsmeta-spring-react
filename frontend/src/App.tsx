51eac0957e68fd5fd347c78300995cf6deb1addc
Marcio André <andremarcio1988@gmail.com>
2022-07-15 09:46:07 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
-import NotificationButton from "./componentes/NotificationButton"
+import Header from "./componentes/Header"
 
+import SalesCard from "./componentes/SalesCard"
 
 function App() {
   return (
     <>
-      <h1> Olá Mundo </h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
